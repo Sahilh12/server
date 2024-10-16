1d07b8c65c2258aa071e32c0f22e83dac8fba31e
Sahilh12 <168436053+Sahilh12@users.noreply.github.com>
2024-10-16 15:40:12 +0530

    set cookie and cors

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -3,11 +3,16 @@ module.exports.jwtToken = async (user, message, statusCode, res) => {
 
     res
         .status(statusCode)
-        .cookie("token", token)
+        .cookie("token", token, {
+            httpOnly: true,      // Ensures the cookie is sent in HTTP requests only (not available to JS)
+            secure: true,        // Cookie will only be sent over HTTPS
+            sameSite: "None",    // Allows cross-site cookie
+        })
         .json({
             success: true,
             token,
             message,
-            user
-        })
+            user,
+        });
+
 }
